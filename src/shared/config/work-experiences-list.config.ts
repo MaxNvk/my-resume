ce3dc71e9d895520849c96d78eb057421b23c976
Max Novikov <max.vashuk@gmail.com>
2024-09-12 18:12:27 -0700

    updated experiences list;

--- src/shared/config/work-experiences-list.config.ts
+++ src/shared/config/work-experiences-list.config.ts
@@ -6,7 +6,14 @@ export interface IWorkExperience {
 
 export const workExperiencesList: IWorkExperience[] = [
   {
-    title: "Milan Art Institute | April 2021 - Present",
+    title: "14Four | October 2023 - Present",
+    description:
+      "Developed creative-focused client-side applications using React (Next) and Vue (Nuxt) for industry-leading brands such as Pepsi, Toyota, and Carmax, etc. Partnered closely with clients to deploy projects seamlessly in their environments, ensuring smooth transitions and providing ongoing support to maintain and optimize their digital platforms.",
+    techStack:
+      "Vue.js, Nuxt.js, SSR, Composition API, TypeScript, WebSocket, Element UI, React.js, Next.js.",
+  },
+  {
+    title: "Milan Art Institute | April 2021 - October 2023",
     description:
       "My primary responsibilities is development of social network platform frontend part and admin panel for it from scratch.",
     techStack:
